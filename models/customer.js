95b700b546d5e0f5e6c701d9deb5142602380c55
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-09-15 11:22:21 -0700

    added fullName() method to grab full name of customer

--- models/customer.js
+++ models/customer.js
@@ -90,6 +90,12 @@ class Customer {
       );
     }
   }
+
+  /** Function to grab full name from customer */
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
 module.exports = Customer;
